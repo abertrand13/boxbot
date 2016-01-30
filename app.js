6a68ec55d6b7612056696b90625be5b4dc41ce92
Alex Bertrand <alex.bertrand13@gmail.com>
2016-01-29 18:10:38 -0800

    Added coin flipping functionality

--- app.js
+++ app.js
@@ -18,36 +18,63 @@ app.post("/groupme", function(req, res) {
 	
 	if(req.body.group_id) {
 
-		console.log("group stage passed");
 		// duck typing.  probably from groupme
 		// check that boxbot was mentioned
 		var msg = req.body.text;
 		var re = /.*@boxbot-test.*/g;
 
 		if(re.test(msg)) {
-			console.log("regex passed");	
-			var headers = {
-				'Content-Type': 'application/json'
-			};
+			
+			var coinFlipRegex = /.*flip.*coin.*/g;
+			if(coinFlipRegex.test(msg)) {	
+				var headers = {
+					'Content-Type': 'application/json'
+				};
 
-			var options = {
-				url		: 'https://api.groupme.com/v3/bots/post',
-				method	: 'POST',
-				headers	: headers,
-				form: {
-					bot_id	: '4b1e5390aee0326d4190116e44',
-					text	: 'Hello Brendan!'
-				}
-			}
+				var cointext = "Flipping... it's " + (Math.random() < .5 ? "HEADS!" : "TAILS!");
 
-			request(options, function(error, response, body) {
-				if(error) {
-					console.log(error);
+				var options = {
+					url		: 'https://api.groupme.com/v3/bots/post',
+					method	: 'POST',
+					headers	: headers,
+					form: {
+						bot_id	: '4b1e5390aee0326d4190116e44',
+						text	: cointext
+					}
 				}
-				if(!error && response.statusCode == 200) {
-					console.log(body);
+
+				request(options, function(error, response, body) {
+					if(error) {
+						console.log(error);
+					}
+					if(!error && response.statusCode == 200) {
+						console.log(body);
+					}
+				});
+			} else {
+				var headers = {
+					'Content-Type': 'application/json'
+				};
+
+				var options = {
+					url		: 'https://api.groupme.com/v3/bots/post',
+					method	: 'POST',
+					headers	: headers,
+					form: {
+						bot_id	: '4b1e5390aee0326d4190116e44',
+						text	: 'Hello Brendan!'
+					}
 				}
-			});
+
+				request(options, function(error, response, body) {
+					if(error) {
+						console.log(error);
+					}
+					if(!error && response.statusCode == 200) {
+						console.log(body);
+					}
+				});
+			}		
 		}
 	}
 
